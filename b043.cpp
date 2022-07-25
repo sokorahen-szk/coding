1d2e4a0709a925a9c387ccc3af9519bd74e01c80
sokorahen-szk <kamonotakahashi@gmail.com>
2022-07-26 02:18:32 +0900

    refactor

--- b043.cpp
+++ b043.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 
 std::string inv(std::string s1) {
-    std::string s2 = ".";
-    return s2 == s1 ? "*" : ".";
+    return "." == s1 ? "*" : ".";
 }
 
 struct postionXY {
@@ -12,47 +11,27 @@ struct postionXY {
 
 void movePos(int scp, int dcp, postionXY &posXY) {
     if (scp == 1) {
-        if (dcp == 2) {
-            posXY.x += 1;
-        }
-        if (dcp == 4) {
-            posXY.x -= 1;
-        }
+        if (dcp == 2) posXY.x += 1;
+        if (dcp == 4) posXY.x -= 1;
     }
     if (scp == 2) {
-        if (dcp == 3) {
-            posXY.y += 1;
-        }
-        if (dcp == 1) {
-            posXY.y -= 1;
-        }
+        if (dcp == 3) posXY.y += 1;
+        if (dcp == 1) posXY.y -= 1;
     }
     if (scp == 3) {
-        if (dcp == 4) {
-            posXY.x -= 1;
-        }
-        if (dcp == 2) {
-            posXY.x += 1;
-        }
+        if (dcp == 4) posXY.x -= 1;
+        if (dcp == 2) posXY.x += 1;
     }
     if (scp == 4) {
-        if (dcp == 1) {
-            posXY.y -= 1;
-        }
-        if (dcp == 3) {
-            posXY.y += 1;
-        }
+        if (dcp == 1) posXY.y -= 1;
+        if (dcp == 3) posXY.y += 1;
     }
 }
 
 void calcPos(int i, int *cp) {
     *cp += i;
-    if (1 > *cp) {
-        *cp = 4;
-    }
-    if (4 < *cp) {
-        *cp = 1;
-    }
+    if (1 > *cp) *cp = 4;
+    if (4 < *cp) *cp = 1;
 }
 
 int main() {
@@ -62,7 +41,6 @@ int main() {
     std::string map[H][W];
     std::string str;
     postionXY posXY = {w0 - 1, h0 - 1};
-    char houseType;
 
     std::string s;
     for(int i = 0; i < H; i++) {
