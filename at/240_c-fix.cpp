4a896343d59480a1774ffd982db95f5949c5f1f9
sokorahen-szk <kamonotakahashi@gmail.com>
2022-08-20 22:54:53 +0900

    fix

--- at/240_c-fix.cpp
+++ at/240_c-fix.cpp
@@ -1,1 +1,29 @@
-TODO:
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+    int N, X;
+    cin >> N >> X;
+    unordered_set<int> st, tm;
+    st.insert(0);
+    for (int i =0; i < N; i++) {
+        int a, b;
+        std::cin >> a >> b;
+        tm.clear();
+        for (auto &c : st) {
+            if (c > X) continue;
+            tm.insert(c + a), tm.insert(c + b);
+        }
+        st = tm;
+    }
+
+    if(st.count(X)) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
